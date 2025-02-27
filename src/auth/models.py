4fdc5aed3ea391182583cdc43f1698322c3c297f
negidnyk <128757470+negidnyk@users.noreply.github.com>
2025-02-27 17:53:41 +0200

    Update models.py

--- src/auth/models.py
+++ src/auth/models.py
@@ -7,36 +7,6 @@ from datetime import date, datetime, time, timedelta
 from database import Base
 
 
-# class Role(Base):
-#     __tablename__ = "role"
-#     __table_args__ = {'extend_existing': True}
-#     id = Column(Integer, primary_key=True, index=True, nullable=False, unique=True)
-#     name = Column(String, nullable=False)
-#     permissions = Column(Integer, nullable=False)
-#
-#
-# class User(SQLAlchemyBaseUserTable[int], Base):
-#     __tablename__ = "borrower"
-#     __table_args__ = {'extend_existing': True}
-#     id = Column(Integer, primary_key=True, index=True, nullable=False, unique=True)
-#     email = Column(String, nullable=False)
-#     username = Column(String, nullable=False)
-#     bio = Column(String(200), nullable=True)
-#     avatar_id = Column(Integer, ForeignKey("files.id"), nullable=True)
-#     registered_at = Column(TIMESTAMP, default=datetime.utcnow)
-#     role_id = Column(Integer, ForeignKey("role.id"))
-#     hashed_password = Column(String, nullable=True)
-#     is_active = Column(Boolean, default=True, nullable=False)
-#     is_superuser = Column(Boolean, default=False, nullable=False)
-#     is_verified = Column(Boolean, default=False, nullable=False)
-#     is_deleted = Column(Boolean, default=False, nullable=False)
-
-
-# @as_declarative()
-# class AbstractModel(SQLAlchemyBaseUserTable[int], Base):
-#     id: Mapped[int] = mapped_column(primary_key=True)
-
-
 class RolesModel(Base):
     __tablename__ = 'roles'
     id: Mapped[int] = mapped_column(primary_key=True)
